5e79365399de3af5c96b73fdb32b908634b423b5
Israel-Laguan <israellaguan@gmail.com>
2019-12-05 18:22:01 -0500

    [fix] mergeability on specs
    What?:
    
    Breaking Changes?

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe HomeController, type: :controller do
 
   describe "GET #index" do
+    # it "returns http success" do
+    #   get :index
+    #   expect(response).to have_http_status(:success)
+    # end
     # subject { get :index }
     # it { should have_http_status(:success) }
   end
