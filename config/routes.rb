22631311b5c7452a07704118ec3784875df4cdf7
Israel-Laguan <israellaguan@gmail.com>
2019-12-05 18:24:22 -0500

    [fix] merge routes
    What?:
    
    Breaking Changes?

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :posts
   resource :home, controller: :home, only: [:index]
   root to: 'posts#index'
+  # root to: 'home#index'
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
