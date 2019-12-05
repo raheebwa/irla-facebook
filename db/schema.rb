d66d74a332ae270e816db416e8ea2eeb49880c08
Aheebwa Ramadhan <raheebwa@hotmail.com>
2019-12-05 19:12:50 +0300

    [feat] Added grravatar gem - Removed gravatar column from database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_04_211624) do
+ActiveRecord::Schema.define(version: 2019_12_05_160917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,6 @@ ActiveRecord::Schema.define(version: 2019_12_04_211624) do
     t.string "encrypted_password", default: "", null: false
     t.string "first_name"
     t.string "last_name"
-    t.string "gravatar"
     t.string "location"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
