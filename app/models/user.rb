2cc61b243b59fd3a142770caae7219dca9ed6abf
Israel-Laguan <israellaguan@gmail.com>
2019-12-06 11:48:42 -0500

    [feat] Model: Add friend, closes #14
    What?:
    - Add model, add migration, add associations
    
    Breaking Changes?

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,37 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy, as: :likeable
 
+  has_many :friendships
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+
   validates :first_name, :last_name, :location, presence: true
 
   # def self.name(user)
   #   "#{user.first_name.capitalize} #{user.last_name.capitalize}"
   # end
+
+  def friends
+    friends_array = friendships.map { |friendship| friendship.friend if friendship.acepted }
+    friends_array.compact + inverse_friendships.map { |friendship| friendship.user if friendship.acepted }
+  end
+
+  # Users who have yet to confirme friend requests
+  def pending_friends
+    friendships.map { |friendship| friendship.friend unless friendship.acepted }.compact
+  end
+
+  # Users who have requested to be friends
+  def friend_requests
+    inverse_friendships.map { |friendship| friendship.user unless friendship.acepted }.compact
+  end
+
+  def confirm_friend(user)
+    friendship = inverse_friendships.find { |friend| friend.user == user }
+    friendship.acepted = true
+    friendship.save
+  end
+
+  def friend?(user)
+    friends.include?(user)
+  end
 end
