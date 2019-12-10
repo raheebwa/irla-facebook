3c46231589283ca79cbccfe73779aec947f4fab3
Israel-Laguan <israellaguan@gmail.com>
2019-12-10 18:29:33 -0500

    [feat] omniauth: Add facebook auth
    What?:
    
    Breaking Changes?

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[facebook]
 
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
@@ -12,4 +13,20 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
 
   validates :first_name, :last_name, :location, presence: true
+
+  def self.from_omniauth(auth)
+    p auth
+    password = Devise.friendly_token[0, 20]
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = password
+      user.password_confirmation = password
+      user.first_name = 'Mr '
+      user.last_name = auth.info.name
+      user.location = 'Unknown'
+      # If you are using confirmable and the provider(s) you use validate emails, 
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
+  end
 end
