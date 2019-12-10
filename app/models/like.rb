5b15c0d6476da84a7e7095a5647472b45c511428
Aheebwa Ramadhan <raheebwa@hotmail.com>
2019-12-10 17:00:44 +0300

    [clean up v2]

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,10 +2,8 @@
 
 class Like < ApplicationRecord
   scope :from_post, ->(post_id) { where post_id: post_id }
-  scope :from_comment, ->(comment_id) { where comment_id: comment_id }
   scope :from_user, ->(user_id) { where user_id: user_id }
-  belongs_to :likeable, polymorphic: true
-  has_one :user
-  has_one :post
-  has_one :comment
+
+  belongs_to :user
+  belongs_to :post
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'faker'
-    categories = []
     User.create([{email: "raaheebwa@gmail.com", first_name: "Aheebwa",
          last_name: "Ramadhan", location: "Uganda", password: "password", password_confirmation: "password"}, 
         {email: "israellaguan@gmail.com", first_name: "Israel", last_name: "Laguan", location: "Colombia" , password: "password", password_confirmation: "password"}])
@@ -16,7 +15,6 @@ require 'faker'
         5.times do 
             post = Post.create(user_id: user.id, body: Faker::ChuckNorris.fact, image_path: Faker::LoremPixel.image(size: "640x480") )
             post.likes.build(user_id: user.id, post_id: post.id).save
-            comment = post.comments.create(user_id: user.id, body: Faker::Hacker.say_something_smart, image_path: Faker::LoremPixel.image(size: "480x260") )
-            comment.likes.build(user_id: user.id, comment_id: comment.id).save
+            3.times{post.comments.create(user_id: user.id, body: Faker::Hacker.say_something_smart)}
         end
     end
