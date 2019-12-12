eacc5bf63611df6453bf164bc140a8112ffb41a2
Israel-Laguan <israellaguan@gmail.com>
2019-12-12 16:54:29 -0500

    [fix] Friendship: improve errors
    What?:
    - Access errors with #errors like
    `$ bad_request.errors`
    
    Breaking Changes?

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -12,12 +12,12 @@ class Friendship < ApplicationRecord
   def if_friend_with_myself
     return unless friend_id == user_id
 
-    errors.add(incorrect_friendship: 'You can not be friend with youlself, duh!')
+    errors.add(:base, 'You can not be friend with youlself, duh!')
   end
 
   def if_reverse_request
     return unless Friendship.where('user_id = ? AND friend_id = ?', friend_id, user_id).any?
 
-    errors.add(incorrect_friendship: 'That friendship is requested or is accepted')
+    errors.add(:base, 'That friendship is requested or is already accepted')
   end
 end
