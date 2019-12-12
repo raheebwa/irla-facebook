a0ab52414112f4d3cd2e0f7756a0d5155636ed27
Israel-Laguan <israellaguan@gmail.com>
2019-12-12 16:22:52 -0500

    [feat] friendships: avoid reverse requests and self friend
    What?:
    
    Breaking Changes?

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,4 +3,21 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+
+  validate :if_friend_with_myself
+  validate :if_reverse_request
+  validates :user_id, presence: true
+  validates :friend_id, presence: true
+
+  def if_friend_with_myself
+    return unless friend_id == user_id
+
+    errors.add(incorrect_friendship: 'You can not be friend with youlself, duh!')
+  end
+
+  def if_reverse_request
+    return unless Friendship.where('user_id = ? AND friend_id = ?', friend_id, user_id).any?
+
+    errors.add(incorrect_friendship: 'That friendship is requested or is accepted')
+  end
 end
