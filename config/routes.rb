e44291ba9ed94ddb68dce09fe7a585996a4b8334
Israel-Laguan <israellaguan@gmail.com>
2019-12-16 14:13:42 -0500

    [feat] Updated User and Post views
    What?:
    
    Breaking Changes?

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   resources :posts
 
   # users
-  resources :users, only: [:index, :show, :edit]
+  resources :users, only: [:show, :edit]
 
   # likes
   resources :likes, only: [:create, :destroy]
